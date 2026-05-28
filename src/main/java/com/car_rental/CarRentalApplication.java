d347d72d5cda7543aab7d494eef347e9624bbd63
Roman <roman.kravchenko1@nure.ua>
2026-05-28 18:01:22 +0200

    CRS-1 User registration

--- src/main/java/com/car_rental/CarRentalApplication.java
+++ src/main/java/com/car_rental/CarRentalApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(DAOConfig.class)
 public class CarRentalApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CarRentalApplication.class, args);
     }
